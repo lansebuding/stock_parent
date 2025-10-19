8dfe4da37c8c9a804ce3b83a559e1630a3c41f7c
YJW <1846770113@qq.com>
2025-10-19 21:37:56 +0800

    day2 swapper练习

--- stock_backend/src/main/java/com/itheima/stock/controller/UserController.java
+++ stock_backend/src/main/java/com/itheima/stock/controller/UserController.java
@@ -6,6 +6,9 @@ import com.itheima.stock.vo.req.LoginReqVo;
 import com.itheima.stock.vo.resp.LoginRespVo;
 import com.itheima.stock.vo.resp.R;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,23 +16,30 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@Api(tags = "用户登录控制器")
 public class UserController {
 
     @Autowired
     private UserServiceImpl userService;
 
     @GetMapping("/user/{name}")
-    SysUser getSysuserByName(@PathVariable String name){
+    @ApiOperation("查询用户信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name", value = "用户名", required = true, paramType = "path")
+    })
+    SysUser getSysuserByName(@PathVariable String name) {
         return userService.getSysUserByName(name);
     }
 
     @PostMapping("/login")
-    R<LoginRespVo> getUser(@RequestBody LoginReqVo reqVo){
+    @ApiOperation("用户登录")
+    R<LoginRespVo> getUser(@RequestBody LoginReqVo reqVo) {
         return userService.getUser(reqVo);
     }
 
     @GetMapping("/captcha")
-    R<Map<String,String>> getCaptcha(){
+    @ApiOperation("获取验证码")
+    R<Map<String, String>> getCaptcha() {
         return userService.getCaptcha();
     }
 }
--- stock_backend/src/main/java/com/itheima/stock/vo/req/LoginReqVo.java
+++ stock_backend/src/main/java/com/itheima/stock/vo/req/LoginReqVo.java
@@ -1,5 +1,7 @@
 package com.itheima.stock.vo.req;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -8,21 +10,26 @@ import lombok.Data;
  * @Description 登录请求vo
  */
 @Data
+@ApiModel(description = "登录请求信息")
 public class LoginReqVo {
     /**
      * 用户名
      */
+    @ApiModelProperty("用户名")
     private String username;
     /**
      * 密码
      */
+    @ApiModelProperty("密码")
     private String password;
     /**
-     * 密码
+     * sessionId
      */
+    @ApiModelProperty("sessionId")
     private String sessionId;
     /**
      * 验证码
      */
+    @ApiModelProperty("验证码")
     private String code;
 }
